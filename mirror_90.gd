a4938ab8e1bab5529c70385cbdc5dcf1c2b0a3eb
mmh132 <mhorwitz@eastsideprep.org>
2023-05-14 22:27:12 -0700

    small housekeeping
    added character, added physics, added tilemap, started playing with mirror pushing

--- mirror_90.gd
+++ mirror_90.gd
@@ -1,5 +1,6 @@
 class_name mirror_90 extends CharacterBody2D
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	#move_and_slide()
