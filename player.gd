50fe74aa926d65c84437f68072bf0d77ff263141
mmh132 <mhorwitz@eastsideprep.org>
2023-05-14 19:10:39 -0700

    added basic functionality to emitter
    worked off an online toutorial, added some basic code to make a lazer wherever my mouse is pointing. 1 weird bug in that a boolean is working the opposite of how it should. I plan to modify the lazer to not follow mouse and instead a predetermined path.

--- player.gd
+++ player.gd
@@ -6,7 +6,43 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready
+var ray = $RayCast2D
+@onready
+var line = $Line2D
 
+var prev = null
+
+func _process(delta):
+	line.clear_points()
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		line.add_point((Vector2(0,0)))
+		ray.global_position = line.global_position
+		ray.target_position = get_global_mouse_position()-line.global_position
+		ray.force_raycast_update()
+	
+		while true:
+			if ray.is_colliding() == false:
+				var pt = ray.global_position + ray.target_position
+				line.add_point(line.to_local(pt))
+				break
+			
+			var collision = ray.get_collider()
+			var pt = ray.get_collision_point()
+			line.add_point(line.to_local(pt))
+			
+			if collision.is_class("mirror"):
+				break	
+			
+			var norm = ray.get_collision_normal()
+			if norm == Vector2(0,0):
+				break		
+			
+			ray.global_position = pt
+			ray.target_position = ray.target_position.bounce(norm)
+			ray.force_raycast_update()
+	pass
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -25,4 +61,4 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	#move_and_slide()
