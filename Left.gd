7caca39f9f06663c56f4c9dd68f64e6f38df1063
mmh132 <mhorwitz@eastsideprep.org>
2023-05-15 22:20:54 -0700

    working level with no textures done
    added push functionality with somewhat messy solution, player is on two layers, area 2ds on second layer that if its colliding with, mirrors move. this layer thing prevents interferance with laser

--- Left.gd
+++ Left.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
-var taken = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	set_monitorable(true)
-		
+	
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	
 	pass
--- mirror_90.gd
+++ mirror_90.gd
@@ -1,8 +1,12 @@
 class_name mirror_90 extends CharacterBody2D
 
+const pushspeed = 100
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready
-var push = $Area2D
+var pushR = $Right
+@onready
+var pushL = $Left
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,6 +19,15 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if pushL.has_overlapping_bodies():
+		print("here")
+		velocity.x -= delta*pushspeed
+	elif pushR.has_overlapping_bodies():
+		velocity.x += delta*pushspeed
+	else: 
+		velocity.x = 0
+		
 	
 	
 	move_and_slide()
